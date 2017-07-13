e1787548b8b1fc2ce069d093d6b42495fca74b19
Hassan-Adel <hasanadel555@hotmail.com>
2017-07-13 12:11:25 +0200

    Adding a Create page for checking account and Validation for it's fields

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,29 @@ namespace AutomatedTellerMachine.Models
     public class CheckingAccount
     {
         public int Id { get; set; }
+
+        [Required]
+        //[StringLength(10, MinimumLength =6)]
+        //Didn'yt use StringLength be I also want to force it to be numeric
+        [RegularExpression(@"\{6, 10}", ErrorMessage = "Account # must be between 6 and 10 digit")]
+        [Display(Name = "Account #")]
         public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name {
+            get {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+        [DataType(DataType.Currency)]
         public decimal Balance { get; set; }
     }
 }
