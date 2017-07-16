5ab9cb41f0f46ed5f0ee02b923838360977fba47
Hassan-Adel <hasanadel555@hotmail.com>
2017-07-16 20:36:04 +0200

    Adding a Transaction model

---
+++ AutomatedTellerMachine/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string CheckingAccountId { get; set; }
+
+        public virtual CheckingAccount CheckingAccount { get; set; }
+
+    }
+}
