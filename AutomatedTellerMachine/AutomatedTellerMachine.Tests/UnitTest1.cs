83aac0b2926f95fa7d5a8199d8177a7e3d7cd364
Hassan-Adel <hasanadel555@hotmail.com>
2017-07-18 02:16:49 +0200

    Adding a simple unit test

---
+++ AutomatedTellerMachine/AutomatedTellerMachine.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutomatedTellerMachine.Controllers;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FooActionReturnsAboutView()
+        {
+            var homeController = new HomeController();
+            var result = homeController.Foo() as ViewResult;
+            Assert.AreEqual("About", result.ViewName);
+        }
+
+        [TestMethod]
+        public void ContactFormSaysThanks()
+        {
+            var homeController = new HomeController();
+            var result = homeController.Contact("I love this bank.") as ViewResult;
+            Assert.IsNotNull(result.ViewBag.TheMessage);
+        }
+    }
+}
