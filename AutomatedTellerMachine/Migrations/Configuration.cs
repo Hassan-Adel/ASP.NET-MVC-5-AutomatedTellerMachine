05822418ede07459d23fedf56a6bf6d455e7e169
Hassan-Adel <hasanadel555@hotmail.com>
2017-07-18 02:00:17 +0200

    Creating Admin user in the seed method

--- AutomatedTellerMachine/Migrations/Configuration.cs
+++ AutomatedTellerMachine/Migrations/Configuration.cs
@@ -1,5 +1,9 @@
 namespace AutomatedTellerMachine.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using Services;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,6 +19,23 @@ namespace AutomatedTellerMachine.Migrations
 
         protected override void Seed(AutomatedTellerMachine.Models.ApplicationDbContext context)
         {
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            //context.Users.AddOrUpdate//in case there's already that user in the DB
+            if(!context.Users.Any(u => u.UserName == "admin@mvcbank.com"))
+            {
+                var adminUser = new ApplicationUser { UserName = "admin@mvcbank.com", Email = "admin@mvcbank.com" };
+                userManager.Create(adminUser, "Admin_2017");
+
+                var service = new CheckingAccountService(context);
+                service.CreateCheckingAccount("admin", "user", adminUser.Id, 0);
+
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Admin" });
+                context.SaveChanges();
+
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
