d75b78599fe0ee78ba1e955146e5f29d0a399bbf
Hassan-Adel <hasanadel555@hotmail.com>
2017-07-21 22:15:57 +0200

    Comment out test transactions

--- AutomatedTellerMachine/Migrations/Configuration.cs
+++ AutomatedTellerMachine/Migrations/Configuration.cs
@@ -37,21 +37,21 @@ namespace AutomatedTellerMachine.Migrations
                 userManager.AddToRole(adminUser.Id, "Admin");
             }
 
-            context.Transactions.Add(new Transaction { Amount = 75, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = -25, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 100000, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 19.99m, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 64.40m, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 100, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = -300, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 255.75m, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 198, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 2, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 50, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = -1.50m, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 6100, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = 164.84m, CheckingAccountId = 9 });
-            context.Transactions.Add(new Transaction { Amount = .01m, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 75, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = -25, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 100000, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 19.99m, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 64.40m, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 100, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = -300, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 255.75m, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 198, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 2, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 50, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = -1.50m, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 6100, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = 164.84m, CheckingAccountId = 9 });
+            //context.Transactions.Add(new Transaction { Amount = .01m, CheckingAccountId = 9 });
 
             //  This method will be called after migrating to the latest version.
 
