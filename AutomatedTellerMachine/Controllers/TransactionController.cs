5b13e2c8064453a724c2f0070e776e319e80bed4
Hassan-Adel <hasanadel555@hotmail.com>
2017-07-21 21:58:02 +0200

    Implementing Transfer Funds

--- AutomatedTellerMachine/Controllers/TransactionController.cs
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -44,6 +44,48 @@ namespace AutomatedTellerMachine.Controllers
             
         }
 
+        // GET: Transaction/Transfer
+        public ActionResult Transfer(int checkingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Transfer(TransferViewModel transfer)
+        {
+            var userCheckingAccount = db.CheckingAccounts.Find(transfer.CheckingAccountId);
+            
+            // check for available funds
+            if (userCheckingAccount.Balance < transfer.Amount)
+            {
+                ModelState.AddModelError("Amount", "You have insufficient funds!");
+            }
+
+            // I'm using first, or default here, so that it will return null if the checking account number isn't valid, instead of throwing an exception.
+            var destinationCheckingAccount = db.CheckingAccounts.Where(c => c.AccountNumber == transfer.DestinationCheckingAccountNumber).FirstOrDefault();
+            
+            // check for a valid destination account
+            if (destinationCheckingAccount == null)
+            {
+                ModelState.AddModelError("DestinationCheckingAccountNumber", "Invalid Destination Checking Account Number !");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(new Transaction { CheckingAccountId = userCheckingAccount.Id , Amount = -transfer.Amount });
+                db.Transactions.Add(new Transaction { CheckingAccountId = destinationCheckingAccount.Id, Amount = transfer.Amount });
+                db.SaveChanges();
+
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(userCheckingAccount.Id);
+                service.UpdateBalance(destinationCheckingAccount.Id);
+
+                return PartialView("_TransferSuccess", transfer);
+            }
+            return PartialView("_TransferForm");
+
+        }
+
         // GET: Transaction/Withdraw
         public ActionResult Withdraw(int checkingAccountId)
         {
